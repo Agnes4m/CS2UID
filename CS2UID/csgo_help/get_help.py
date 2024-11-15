d6472c4571c8a032acdd14755ee16e65ef227cda
Agnes Digital <735803792@qq.com>
2024-11-15 16:35:30 +0800

    🐛修复在没有前缀下读取help错误的bug

--- CS2UID/csgo_help/get_help.py
+++ CS2UID/csgo_help/get_help.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import aiofiles
 from PIL import Image
 from msgspec import json as msgjson
-from gsuid_core.sv import get_plugin_prefix
+from gsuid_core.sv import get_plugin_available_prefix
 from gsuid_core.help.model import PluginHelp
 from gsuid_core.help.draw_new_plugin_help import get_new_help
 
@@ -15,8 +15,7 @@ ICON_PATH = Path(__file__).parent / 'icon_path'
 TEXT_PATH = Path(__file__).parent / 'texture2d'
 ICON = TEXT_PATH / 'icon.png'
 
-PREFIX = get_plugin_prefix('CS2UID')
-
+PREFIX = get_plugin_available_prefix('CS2UID')
 
 def get_footer():
     return Image.open(TEXT_PATH / 'footer.png')
